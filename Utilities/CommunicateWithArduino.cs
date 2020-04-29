116c1e15fb6515d29fce07562f0f8106825c416e
wen-jie.tseng <wen-jie.tseng@telecom-paris.fr>
2020-04-29 12:42:39 +0800

    add serial port and spherical to catersian

---
+++ Utilities/CommunicateWithArduino.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.IO.Ports;
+using System.Threading;
+
+public class CommunicateWithArduino
+{
+    private string portName;
+    private int baudRate;
+    private Parity parity;
+    private int dataBits;
+    private StopBits stopBits;
+    private Handshake handshake;
+    private bool RtsEnable;
+    private int ReadTimeout;
+    private bool isMac;
+    private bool isConnected;
+    private string motorFinishMessage = "";
+    private DateTime getFinishMessageTime = DateTime.Now;
+    private SerialPort arduinoController;
+
+    public CommunicateWithArduino(string portName, int baudRate = 9600, Parity parity = Parity.None, int dataBits = 8, StopBits stopBits = StopBits.One, Handshake handshake = Handshake.None,
+        bool RtsEnable = true, int ReadTimeout = 50, bool isMac = false, bool isConnected = true)
+    {
+        this.portName = portName;
+        this.baudRate = baudRate;
+        this.parity = parity;
+        this.dataBits = dataBits;
+        this.stopBits = stopBits;
+        this.handshake = handshake;
+        this.RtsEnable = RtsEnable;
+        this.ReadTimeout = ReadTimeout;
+        this.isMac = isMac;
+        this.isConnected = isConnected;
+        startConnect();
+    }
+
+    private void startConnect()
+    {
+        if (isConnected)
+        {
+            if (isMac)
+            {
+                int p = (int)Environment.OSVersion.Platform;
+                // Are we on Unix?
+                if (p == 4 || p == 128 || p == 6)
+                {
+                    List<string> serial_ports = new List<string>();
+                    string[] ttys = Directory.GetFiles("/dev/", "cu.*");
+                    foreach (string dev in ttys)
+                    {
+                        if (dev.StartsWith("/dev/tty."))
+                        {
+                            serial_ports.Add(dev);
+                            Debug.Log(String.Format(dev));
+                        }
+                    }
+                }
+                portName = "/dev/" + portName;
+            }
+            arduinoController = new SerialPort(portName, baudRate, parity, dataBits, stopBits);
+            arduinoController.Handshake = handshake;
+            arduinoController.RtsEnable = RtsEnable;
+            arduinoController.ReadTimeout = ReadTimeout;
+            arduinoController.Open();
+        }
+    }
+    public void sendData(object obj)
+    {
+        string data = obj as string;
+        Debug.Log(data);
+        if (isConnected && arduinoController != null)  //&& !isLocked
+        {
+            
+            arduinoController.DiscardInBuffer();       //clear buffer
+            arduinoController.DiscardOutBuffer();     //clear buffer
+            arduinoController.Write(data);
+            arduinoController.Write("\n");
+            getFinishMessageTime = DateTime.Now;
+            Thread.Sleep(500);
+        }
+        else
+        {
+            //Debug.LogError("Not Connected or Locked");
+        }  
+    }
+
+    public string receiveData()
+    {
+        string message;
+        try
+        {
+            message = arduinoController.ReadLine();
+        }
+        catch (TimeoutException)
+        {
+            message = ""; 
+        }
+                
+        return message;
+        //return arduinoController.ReadLine();
+    }
+}
---
+++ Utilities/namespace SphericalCoordinatesConvert.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SphericalCoordinatesConvert
+{
+    public class SphericalCoordinates
+    {
+        /// <summary>
+        /// Store spherical coordinate, spherical to cartesian and vice versa
+        /// http://en.wikipedia.org/wiki/Spherical_coordinate_system
+        /// https://blog.nobel-joergensen.com/2010/10/22/spherical-coordinates-in-unity/
+        /// </summary>
+
+        public float radius;    // radius >= 0, from a fixed origin
+        public float polar;     // azimuth angle (in radian)
+        public float elevation; // elevation angle (in radian)
+
+        public Vector3 ToCartesian()
+        {
+            return SphericalToCartesian(radius, polar, elevation);
+        }
+
+        public static SphericalCoordinates ToSpherical(Vector3 cartCoord)
+        {
+            SphericalCoordinates store = new SphericalCoordinates();
+            CartesianToSpherical(cartCoord, out store.radius, out store.polar, out store.elevation);
+            return store;
+        }
+
+        public static Vector3 SphericalToCartesian(float radius, float polar, float elevation)
+        {
+            Vector3 outCart = new Vector3();
+            float a = radius * Mathf.Cos(elevation);
+            outCart.x = a * Mathf.Cos(polar);
+            outCart.y = radius * Mathf.Sin(elevation);
+            outCart.z = a * Mathf.Sin(polar);
+            return outCart;
+        }
+
+        public static void CartesianToSpherical(Vector3 cartCoord, out float outRadius, out float outPolar, out float outElevation)
+        {
+            if (cartCoord.x == 0) cartCoord.x = Mathf.Epsilon;
+            outRadius = Mathf.Sqrt(cartCoord.x * cartCoord.x + cartCoord.y * cartCoord.y + cartCoord.z * cartCoord.z);
+            outPolar = Mathf.Atan2(cartCoord.z, cartCoord.x); // otherwise x and z would have negative
+            if (cartCoord.x < 0) outPolar += Mathf.Epsilon;
+            outElevation = Mathf.Asin(cartCoord.y / outRadius);
+        }
+    }
+    // // sepherical coord to cartesian coord (old)
+    // // x -> x, y -> z, z -> y (Wiki to Unity)
+    // Vector3 realHand2origin = realHand.position - origin.position;
+    // // working on the correctness of this GOGO
+    // // Perhaps GOGO is not good for the translation in our case;
+    // phi = Mathf.Atan2(realHand2origin.z, realHand2origin.x);
+    // theta = Mathf.Acos(realHand2origin.y / R_real);
+    // float x = R_virtual * Mathf.Sin(theta) * Mathf.Cos(phi);
+    // float z = R_virtual * Mathf.Sin(theta) * Mathf.Sin(phi); // wrong
+    // float y = R_virtual * Mathf.Cos(theta);
+    // virtualHand.transform.localPosition = new Vector3(x, y, z); // consider localPosition, different from all global variables, did not consider rotation/eulerAngle/quaternion
+}
